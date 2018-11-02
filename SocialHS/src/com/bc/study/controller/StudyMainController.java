b559284aad21e510e025b61801f2754c1893b2ab
허동우 <@DESKTOP-L0A47T0>
2018-11-02 11:30:22 +0900

    error 수정

--- SocialHS/src/com/bc/study/controller/StudyMainController.java
+++ SocialHS/src/com/bc/study/controller/StudyMainController.java
@@ -15,7 +15,7 @@ import com.bc.share.command.Command;
 
 
 
-@WebServlet("/StudyMainController")
+@WebServlet("/StudyController")
 public class StudyMainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
