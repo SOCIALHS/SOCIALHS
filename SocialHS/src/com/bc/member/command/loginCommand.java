1e3fb869b23caa442d44ae71e5df26bb97d2dd4f
11 <11@hb5018>
2018-11-05 11:52:19 +0900

    로그인 헤더 수정

--- SocialHS/src/com/bc/member/command/loginCommand.java
+++ SocialHS/src/com/bc/member/command/loginCommand.java
@@ -15,7 +15,6 @@ public class loginCommand implements Command {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String path = null;
-		boolean log_check = false;
 		
 		HttpSession session = request.getSession();
 		memberDAO dao = new memberDAO();
@@ -24,19 +23,15 @@ public class loginCommand implements Command {
 		if (chk == 0) {
 			//비밀번호가 틀릴 경우->로그인 화면 이동
 			System.out.println("비밀번호를 확인해주세요.");
-			session.setAttribute("log_check", false);
 			path = "index.jsp";
 		} else if (chk == -1) {
 			//아이디가 없을 경우->로그인 화면 이동
 			System.out.println("존재하지 않는 아이디입니다.");
-			session.setAttribute("log_check", false);
 			path = "index.jsp";
 		} else {
 			//로그인 성공 -> 메인화면 이동
 			System.out.println("로그인 성공!");
-			//session.setAttribute("memberid", id);
-			session.setAttribute("log_check", true);
-			System.out.println(log_check);
+			session.setAttribute("memberid", id);
 			path = "index.jsp";
 		}
 		
