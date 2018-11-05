e3bf567de7351fada6b8825a215626431a71e20d
shinmingyeong <shinmingyeong@192.168.0.35>
2018-11-05 16:49:18 +0900

    마이페이지 로그인로그아웃

--- SocialHS/WebContent/jieun/loginheader.jsp
+++ SocialHS/WebContent/jieun/loginheader.jsp
@@ -121,7 +121,6 @@
 </head>
 
 <body>
-<% session.getAttribute("memberVO"); %>
 
     <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" style="color:aliceblue;">SHAS (Social Hobby & Study)</a>
--- SocialHS/src/com/bc/member/command/loginCommand.java
+++ SocialHS/src/com/bc/member/command/loginCommand.java
@@ -1,5 +1,6 @@
 package com.bc.member.command;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -19,7 +20,7 @@ public class loginCommand implements Command {
 		
 		HttpSession session = request.getSession();
 		memberDAO dao = new memberDAO();
-		memberVO vo = new memberVO();
+		memberVO vo = dao.selectId(id);
 		int chk = dao.loginCheck(id, pw);
 			
 		if (chk == 0) {
@@ -34,6 +35,7 @@ public class loginCommand implements Command {
 			//로그인 성공 -> 메인화면 이동
 			System.out.println("로그인 성공!");
 			session.setAttribute("memberid", id);
+			session.setAttribute("memberVO", vo);
 			path = "index.jsp";
 		}
 		
