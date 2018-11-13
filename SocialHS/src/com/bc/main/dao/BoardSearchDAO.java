9875dedeeb325866687a19b4abacdb95d8bf79b8
six6929 <six6929@naver.com>
2018-11-13 17:35:10 +0900

    search

--- SocialHS/src/com/bc/main/dao/BoardSearchDAO.java
+++ SocialHS/src/com/bc/main/dao/BoardSearchDAO.java
@@ -52,4 +52,38 @@ public class BoardSearchDAO {
 		return c_list;
 	}
 	
+	public static List<BoardVO> getTitle2(String hs, String keyword, String sl_idx) {
+
+		Map<String, String> map = new HashMap<>();
+		map.put("hs", hs);
+		map.put("keyword", keyword);
+		map.put("sl_idx", sl_idx);
+		
+		System.out.println("getTitle2 map : " + map);
+		List<BoardVO> t_list = getSql().selectList("search.title2", map);
+		return t_list;
+	}
+	
+	
+	public static List<BoardVO> getId2(String hs, String keyword, String sl_idx){
+		Map<String, String> map = new HashMap<>();
+		map.put("hs", hs);
+		map.put("keyword", keyword);
+		map.put("sl_idx", sl_idx);
+		
+		System.out.println("getId2 map : " + map);
+		List<BoardVO> i_list =  getSql().selectList("search.id2", map);
+		return i_list;
+	}
+	
+	public static List<BoardVO> getContent2(String hs, String keyword, String sl_idx){
+		Map<String, String> map = new HashMap<>();
+		map.put("hs", hs);
+		map.put("keyword", keyword);
+		map.put("sl_idx", sl_idx);
+		
+		System.out.println("getContent2 map : " + map);
+		List<BoardVO> c_list = getSql().selectList("search.content2", map);
+		return c_list;
+	}
 }
